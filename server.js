14e6db872be8e6177e040b5c0ce203b9a23e3c38
sdeagh <huw.burton@btinternet.com>
2018-04-16 14:13:32 +0100

    fatoring code

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const knex = require('knex');
 const bcrypt = require('bcrypt-nodejs');
 const register = require('./controllers/register');
+const signin = require('./controllers/signin')
 
 const db = knex({
     client: 'pg',
@@ -21,26 +22,7 @@ app.get('/', (req, res) => {
     res.send('working...')
 })
 
-app.post('/signin', (req, res) => {
-    db.select('email', 'hash')
-        .from('login')
-        .where('email', req.body.email)
-        .then(data => {
-            const isValid = bcrypt.compareSync(req.body.password, data[0].hash)
-            if (isValid) {
-                return db.select('*')
-                    .from('users')
-                    .where('email', data[0].email)
-                    .then(user => {
-                        res.json(user[0])
-                    })
-                    .catch(err => res.status(400).json('Unable to get user'))
-            } else {
-                res.status(400).json('Invalid login')
-            }
-        })
-        .catch(err => res.status(400).json('Wrong credentials'))
-})
+app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
 
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
 
