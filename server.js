66d1b3039184f69c3bbe9c1a0f36c55b91711719
sdeagh <huw.burton@btinternet.com>
2018-04-16 15:28:22 +0100

    fatoring code

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const knex = require('knex');
 const bcrypt = require('bcrypt-nodejs');
 const register = require('./controllers/register');
 const signin = require('./controllers/signin')
+const profile = require('./controllers/profile')
+const image = require('./controllers/image')
 
 const db = knex({
     client: 'pg',
@@ -26,31 +28,9 @@ app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
 
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
 
-app.get('/profile/:id', (req, res) => {
-    const  { id } = req.params;
-    db.select('*').from('users').where('id', id)
-        .then(user => {
-            if (user.length) {
-                res.json(user[0])
-            } else {
-                res.status(400).json('User not found')
-            }
-        })
-        .catch(err => res.status(400).json("Error retrieving user"))
-})
+app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
 
-app.put('/image', (req, res) => {
-    const { id } = req.body;
-    db('users')
-        .where('id', '=', id)
-        .increment('entries', 1)
-        .returning('entries')
-        .then(entries => {
-            res.json(entries[0])
-        })
-        .catch(err => res.status(400).json("Unable to get entries"))
-    
-})
+app.put('/image', (req, res) => {image.imageCount(req, res, db)})
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`app is running on port ${process.env.PORT}`)
